04c4d3ba939043d5d6dba4ddff0cf1c421f71224
SungSooJung <meseji87@gmail.com>
2016-08-28 19:27:56 +0900

    2016-08-22

--- ReverseComplement/main.c
+++ ReverseComplement/main.c
@@ -1,49 +1,49 @@
 #include <stdio.h>
-void setInitArr();
-void setTwoCom(int n);
+void setInitArr(int *arr);
+int setTwoCom(int n);
+void printBit(int *arr);
 
 int arr[8];
+const int MAX_SIZE =8;           
 
 
-void setTwoCom(int n){
-	setInitArr();
+int setTwoCom(int n){
+	setInitArr(arr);
 	int num = n;
 	int rest,i,point;
-	//int cur=(sizeof(arr)/sizeof(int))-1;
 	int cur = 0;
 	do{
 		point=num/2;
 		arr[cur++]=num%2;
 		num=point;
 	}while(point>0);
-}
-void reverseCom(){
-	int i;
 
-	for (i=0;i<sizeof(arr)/sizeof(int);i++){
-
-		arr[i]= ( arr[i]==1 ) ? 0:1;
-	}
+//	printBit(arr);
+	return getValue(arr);
 }
-void setInitArr(){
+void setInitArr(int *arr){
 	int i;
-	for (i=0;i<sizeof(arr)/sizeof(int);i++){
+	for (i=0;i<MAX_SIZE;i++){
 		arr[i]=0;
 	}
 }
-int getValue(){
+int getEndIndex(int *arr){
+	int i;
+	for (i=MAX_SIZE-1;i>=0;i--){
+		if (arr[i]==1){
+			break;
+		}
+	}	
+	return i;
+}
+int getValue(int *arr){
 	int i,j;
 	int end;
 	int v;
 	int tmp;
-	int size;
-	for (i=sizeof(arr)/sizeof(int);i>=0;i--){
-		if (arr[i]==1){
-			end=i;
-			break;
-		}
-	}	
-	size=2;	
+	int size=2;
+	end = getEndIndex(arr);
+	v=0;
 	for (i=end;i>=0;i--){
 		tmp= (arr[i]==1)?0:1;
 		if (i==end){
@@ -59,7 +59,7 @@ int getValue(){
 	return v;
 }
 
-void printArr(){
+void printBit(int *arr){
 	int i;
 	for (i=0;i<sizeof(arr)/sizeof(int);i++){
 		printf("%d ",arr[i]);
@@ -71,11 +71,9 @@ void printArr(){
 
 
 int main(void){
-	int i;
-	setTwoCom(27);	
-//	reverseCom();
-	printArr();
-	printf("value:%d\n",getValue());
+	printf("%d\n",setTwoCom(37));
+	printf("%d\n",setTwoCom(54));
+	printf("%d\n",setTwoCom(27));
 
 	return 0;
 }
