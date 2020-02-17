0254fa002c571119b82bab1fb08334cea3a74842
meseji87@gmail.com <wjdtjdtn*7>
2020-02-18 00:48:37 +0900

    [20200218] 스터디 문제 업로드

--- problem-solving/baekjoon/Baekjoon11048/main.py
+++ problem-solving/baekjoon/Baekjoon11048/main.py
@@ -1,45 +1,79 @@
-
+import sys
+import random
+sys.setrecursionlimit(100000)
 
 maxCandyCnt=0
-N=4
-M=3
+N=500
+M=500
+isFirst=True
+cmap=[]
+call_pos=dict()
+call_cnt=0
 
 def inputdata():
-    N,M=int(input().split(' '))
-    cmap=[]
+    global cmap,N,M
+
+    in_first=input().split(' ')
+    N=int(in_first[0])
+    M=int(in_first[1])
+#    cmap=[]
+    cmap=[0 for i in range(0,N)]
     for i in range(0,N):
-        list=[input().split(' ')]
-        cmap[i]=list
+        in_val=input()
+        map_val=in_val.split(' ')
+        cmap[i]=map_val
+
+    #print(cmap)
     
-def travelMap(_cmap,curPos,candycnt):
-    global maxCandyCnt
+def travelMap(curPos,candycnt,travelPath):
+    global maxCandyCnt,isFirst,call_pos,cmap,call_cnt
+    call_cnt+=1
     x,y=curPos
-    if x < 0 or y < 0 or x >= N or y >=M:
+
+    if x < 0 or y < 0 or x >= (M) or y >= (N):
         return
-    elif (x+1)==N and (y+1)==M:
-        candycnt+=_cmap[x][y]
+    elif (x)==(M-1) and (y)==(N-1):
+        if travelPath in call_pos.keys():
+            return
+        else:
+            call_pos[travelPath]=True
+            #print(travelPath)
+        candycnt+=int(cmap[x][y])
+        if isFirst:
+            maxCandyCnt=candycnt
+            isFirst=False
+
         if maxCandyCnt <= candycnt:
             maxCandyCnt=candycnt
+        #print('current_candy {}'.format(candycnt))
         return
     else:
-        candycnt+=_cmap[x][y]
-        travelMap(_cmap,[x,y+1],candycnt)
-        travelMap(_cmap,[x+1,y],candycnt)
-        travelMap(_cmap,[x+1,y+1],candycnt)
-    
+  
+        travelPath+=str(x)+str(y)
+        candycnt+=int(cmap[x][y])
+        travelMap([x,y+1],candycnt,travelPath)
+        travelMap([x+1,y],candycnt,travelPath)
+        travelMap([x+1,y+1],candycnt,travelPath)
 
 def solution(cmap):
-    travelMap(cmap,[0,0],0)
-    print("max = ",maxCandyCnt)
+    travelMap([0,0],0,'')
+#    print("max = ",maxCandyCnt)
+    print(maxCandyCnt)
 
 def main():
+    global cmap
     #cmap=[[1,0,0],[0, 1, 0],[0, 0 ,1]]  
-    #cmap=[[1,2, 3, 4],[0, 0, 0, 5],[9, 8, 7, 6]]     
-    cmap=[[1, 2, 3],[6, 5, 4],[7, 8, 9],[12, 11, 10]]
+    #cmap=[[1, 2, 3, 4],[0, 0, 0, 5],[9, 8, 7, 6]]     
+    #cmap=[[1, 2, 3],[6, 5, 4],[7, 8, 9],[12, 11, 10]]
+#    inputdata()
+    cmap=[[random.randint(0,10) for i in range(0,100)] for i in range(0,100)]
+
     solution(cmap)
+
 
 
 
 if __name__=="__main__":
     main()
-    #inputdata()
+    print(call_cnt)
+#print(call_pos)
